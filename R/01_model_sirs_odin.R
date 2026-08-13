2b786b10e49ea39bce48ee4b77864fe2299cd421
Kate <k.turpie24@imperial.ac.uk>
2026-08-13 14:21:09 +0100

    learning git :(

--- R/01_model_sirs_odin.R
+++ R/01_model_sirs_odin.R
@@ -4,7 +4,7 @@
 
 # Purpose : Defines the core age-structured RSV dynamic transmission model
 # By : Kate Turpie
-# Updated : 07-July-26
+# Updated : 09-July-26
 
 
 # ---------------------------------------------------------------------------- #
